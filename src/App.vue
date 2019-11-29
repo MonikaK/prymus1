21e2b159c8c773fa0da285cff03be71e575bb40c
Student <student@d17-l427-pc-17.ipa.ki.agh.edu.pl>
2019-11-29 17:51:20 +0100

    lab4

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,28 @@
 <template>
-  <div>  
-    Firstname<input type="text" v-model="Firstname"><br>
+   <div>
+    <h1>New participant</h1>
+     Firstname<input type="text" v-model="Firstname"><br>
     Lastname<input type="text" v-model="Lastname"><br>
-    <button @click="addNew()">Add new</button>
-    <ul v-if="list.length>0">
-      <li v-for="el in list">{{el}}</li>
-    </ul>
-  </div>
+    <button @click="addNew()">Add new participant</button>
+    <!-- <new-participant-form @added="addNewParticipant($event)"></new-participant-form> -->
+   <participants-list :list="list"></participants-list>
+    </div>
 </template>
- 
+
 <script>
+import ParticipantsList from "./ParticipantsList.vue";
+import NewParticipantsList from "./NewParticipantsList.vue";
     export default {
+      components: {ParticipantsList, NewParticipantsList},
         data() {
-            return {list: []}
+            return {list: [],
+            Firstname: "",
+            Lastname: ""}
         },
         methods: {
           addNew() {
              this.list.push(this.Firstname + this.Lastname);
+             this.ParticipantsList
           }
         }
     };
