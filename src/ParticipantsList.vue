0a8fd95b3df6c60c9d2344758256ff54ace5c146
MonikaK <57507892+MonikaK@users.noreply.github.com>
2019-12-13 12:37:52 +0100

    Create ParticipantsList.vue

---
+++ src/ParticipantsList.vue
@@ -0,0 +1,17 @@
+@ -0,0 +1,16 @@
+<template>
+  <div>
+    <ol>
+        <li v-for="person in list">
+          {{person}}
+          <button>Remove</button>
+        </li>
+    </ol>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['list']
+  };
+</script>
