620a084d7e2a52ab134b0f8cf048ecbd992ac58b
MonikaK <57507892+MonikaK@users.noreply.github.com>
2019-12-13 12:40:07 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,38 @@
+@@ -1,28 +1,26 @@
 <template>
-  <div>  
-    Firstname<input type="text" v-model="Firstname"><br>
+   <div>
+    <h1>New participant</h1>
+     Firstname<input type="text" v-model="Firstname"><br>
     Lastname<input type="text" v-model="Lastname"><br>
-    <button @click="addNew()">Add new</button>
-    <ul v-if="list.length>0">
-      <li v-for="el in list">{{el}}</li>
-    </ul>
-  </div>
+    <button @click="addNew()">Add new participant</button>
+    <!-- <new-participant-form @added="addNewParticipant($event)"></new-participant-form> -->
+    <h1>Participants list</h1>
+    <participants-form @added="addNew($event)"></participants-form>
+   <participants-list :list="list"></participants-list>
+    </div>
 </template>
- 
+
+
 <script>
+import ParticipantsList from "./ParticipantsList.vue";
+import NewParticipantsList from "./NewParticipantsList.vue";
+import ParticipantsForm from "./ParticipantsForm.vue";
     export default {
+      components: {ParticipantsList, NewParticipantsList},
+      components: {ParticipantsList, ParticipantsForm},
         data() {
-            return {list: []}
+            return {list: [],
+            Firstname: "",
+            Firstname: "" ,
+            Lastname: ""}
         },
         methods: {
           addNew() {
              this.list.push(this.Firstname + this.Lastname);
+             this.ParticipantsList
+          addNew(Person) {
+            console.log(Person);
+             this.list.push(Person);
           }
         }
     };
-</script>
