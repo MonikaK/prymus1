8c150504ee365f9fded77d3b888506bb7ab72a38
MonikaK <57507892+MonikaK@users.noreply.github.com>
2019-12-13 12:46:58 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,6 @@
 <template>
    <div>
     <h1>New participant</h1>
-     Firstname<input type="text" v-model="Firstname"><br>
-    Lastname<input type="text" v-model="Lastname"><br>
-    <button @click="addNew()">Add new participant</button>
     <!-- <new-participant-form @added="addNewParticipant($event)"></new-participant-form> -->
     <h1>Participants list</h1>
     <participants-form @added="addNew($event)"></participants-form>
@@ -15,21 +12,17 @@
 
 <script>
 import ParticipantsList from "./ParticipantsList.vue";
-import NewParticipantsList from "./NewParticipantsList.vue";
 import ParticipantsForm from "./ParticipantsForm.vue";
     export default {
-      components: {ParticipantsList, NewParticipantsList},
       components: {ParticipantsList, ParticipantsForm},
         data() {
             return {list: [],
-            Firstname: "",
             Firstname: "" ,
             Lastname: ""}
         },
         methods: {
           addNew() {
-             this.list.push(this.Firstname + this.Lastname);
-             this.ParticipantsList
+             this.list.push(this.Firstname + ' ' + this.Lastname);
           addNew(Person) {
             console.log(Person);
              this.list.push(Person);
