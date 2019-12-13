466723039ee72616c22ed9fc2fb0c7b93b9449b4
MonikaK <monika-kidawska@wp.pl>
2019-12-13 12:12:31 +0100

    Revert "strona"
    This reverts commit 51bfd58fc7edd894b8f76ce9439b32689517a466.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
    <div>
-    <h1>Participants list</h1>
-    <participants-form @added="addNew($event)"></participants-form>
+    <h1>New participant</h1>
+     Firstname<input type="text" v-model="Firstname"><br>
+    Lastname<input type="text" v-model="Lastname"><br>
+    <button @click="addNew()">Add new participant</button>
+    <!-- <new-participant-form @added="addNewParticipant($event)"></new-participant-form> -->
    <participants-list :list="list"></participants-list>
     </div>
 </template>
 
-
 <script>
 import ParticipantsList from "./ParticipantsList.vue";
-import ParticipantsForm from "./ParticipantsForm.vue";
+import NewParticipantsList from "./NewParticipantsList.vue";
     export default {
-      components: {ParticipantsList, ParticipantsForm},
+      components: {ParticipantsList, NewParticipantsList},
         data() {
             return {list: [],
-            Firstname: "" ,
+            Firstname: "",
             Lastname: ""}
         },
         methods: {
-          addNew(Person) {
-            console.log(Person);
-             this.list.push(Person);
+          addNew() {
+             this.list.push(this.Firstname + this.Lastname);
+             this.ParticipantsList
           }
         }
     };
