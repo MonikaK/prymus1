6df1b573165812997efce88adccb402b347db6d4
MonikaK <monika-kidawska@wp.pl>
2019-12-13 12:14:41 +0100

    Revert "lab4"
    This reverts commit 21e2b159c8c773fa0da285cff03be71e575bb40c.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
-   <div>
-    <h1>New participant</h1>
-     Firstname<input type="text" v-model="Firstname"><br>
+  <div>  
+    Firstname<input type="text" v-model="Firstname"><br>
     Lastname<input type="text" v-model="Lastname"><br>
-    <button @click="addNew()">Add new participant</button>
-    <!-- <new-participant-form @added="addNewParticipant($event)"></new-participant-form> -->
-   <participants-list :list="list"></participants-list>
-    </div>
+    <button @click="addNew()">Add new</button>
+    <ul v-if="list.length>0">
+      <li v-for="el in list">{{el}}</li>
+    </ul>
+  </div>
 </template>
-
+ 
 <script>
-import ParticipantsList from "./ParticipantsList.vue";
-import NewParticipantsList from "./NewParticipantsList.vue";
     export default {
-      components: {ParticipantsList, NewParticipantsList},
         data() {
-            return {list: [],
-            Firstname: "",
-            Lastname: ""}
+            return {list: []}
         },
         methods: {
           addNew() {
              this.list.push(this.Firstname + this.Lastname);
-             this.ParticipantsList
           }
         }
     };
